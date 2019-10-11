841e936a3e679c863d06c0f96d4bf965592450f2
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-11 13:51:38 -0700

    application.properties and pom.xml set up

---
+++ sprinttwo/src/main/java/local/raajn/sprint/sprinttwo/SprinttwoApplication.java
@@ -0,0 +1,15 @@
+package local.raajn.sprint.sprinttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinttwoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SprinttwoApplication.class, args);
+    }
+
+}

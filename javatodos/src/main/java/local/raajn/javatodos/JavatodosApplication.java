a2180ac48fb5a676e7a9673333fcd8356d2f8107
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-11 08:26:20 -0700

    project initiated. spring initialized app created

---
+++ javatodos/src/main/java/local/raajn/javatodos/JavatodosApplication.java
@@ -0,0 +1,15 @@
+package local.raajn.javatodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatodosApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavatodosApplication.class, args);
+    }
+
+}

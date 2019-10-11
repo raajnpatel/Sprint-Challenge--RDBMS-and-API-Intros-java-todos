f0cfe378b4d4cdbbd7d88df0f2ac2877a901685f
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-11 12:33:22 -0700

    Project reinitiated.

---
+++ todosjava/src/main/java/local/raajn/todosjava/TodosjavaApplication.java
@@ -0,0 +1,15 @@
+package local.raajn.todosjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosjavaApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TodosjavaApplication.class, args);
+    }
+
+}
